0b147108e5241ea8da021376d70811e82310968e
Даник Курносов <malouw211@gmail.com>
2023-01-30 15:45:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// Задача 1
+
+//let i = 0;
+
+//while(i < 50) {
+ //     i++;
+//      console.log(i)  
+//}
+//
+//for (i=35;i>8;i--) {
+//    console.log(i);
+//    }
+
+// Задача 2
+
+//var i = 89;
+//while (i > 11) {
+//	document.write(i + '<br>');
+//	i--;
+//}
+
+// Задача 3
+
+//let res = 0;
+
+//for ( let i = 0; i <= 100; i++) {
+//	res = res + i;
+//}
+
+//console.log(res); 
+
+// Задача 4
+
+//let sum = 0;
+//for (let i = 1; i <= 5; i++) {
+ //   sum += i;
+ //   console.log(sum);
+//}
+
+// Задача 5
+//for (let i = 8; i <= 56; i++) {
+   // if (i % 2 == 0) {
+   //   console.log( i );
+   // }
+ // }
+
+ // Задача 7
+
+
+ //let n = 1000;
+
+//let i = 0;
+
+//while (n > 50) {
+
+
+ // n /= 2;
+
+
+//i++;
+
+//}
+
+//console.log(n); 
+
+//console.log(i);
+
+
+
+
+
+
